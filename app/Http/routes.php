af6c09fc6d1d31d45c251c3e6c40190d9a00c129
Salahuddin Hairai <mr.od3n@gmail.com>
2015-07-29 15:39:36 +0800

    Activate Authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,5 +11,14 @@
 |
 */
 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('/', ['as' => 'home', 'uses' => 'WelcomeController@index']);
 Route::controllers(['lists' => 'ListsController']);
