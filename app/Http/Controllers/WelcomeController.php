eca35cbb13db288c2cf568c6f61d166b490e7f6f
Salahuddin Hairai <mr.od3n@gmail.com>
2015-07-29 11:49:32 +0800

    Update Welcome

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,17 +1,18 @@
-<?php App\Http\Controlers;
+<?php namespace App\Http\Controllers;
 
 class WelcomeController extends Controller {
 
     public function index() {
 
         $items = array(
-            $items => [
+            'items' => [
                 'Pack luggage',
                 'Go to airpot',
                 'Arrive in Penang'
-            ];
-            )
+            ]
+        );
 
+        dd($items);
         return view('welcome');
     }
 }
