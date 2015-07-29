056ed0bdc5782bec3861d8d0a965a480c0bfae8a
Salahuddin Hairai <mr.od3n@gmail.com>
2015-07-29 14:44:34 +0800

    Looping over an Array & If conditional

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -1,5 +1,16 @@
 <ul>
-	@foreach ($lists as $list)
+<!-- 	@forelse ($lists as $list)
 		<li>{{ $list }}</li>
-	@endforeach	 
+	@empty
+		<li>You don't have any lists saved.</li>
+	@endforelse
+ -->
+	@if (count($lists) > 0)
+		@foreach ($lists as $list) 
+			<li>{{ $list }}</li>
+		@endforeach
+	@else
+		<li>You don't have any lists saved.</li>
+	@endif
+
 </ul>
