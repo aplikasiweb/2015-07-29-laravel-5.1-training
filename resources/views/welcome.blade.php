024959e0a864c58c9ee553d8007942e598d73efe
Salahuddin Hairai <mr.od3n@gmail.com>
2015-07-29 15:57:23 +0800

    Update welcome template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,11 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">Welcome to TODO</div>
 				<div class="panel-body">
-					
+					@if (Auth::id())
+						You are logged in!
+					@else
+						Please login to view your list!
+					@endif 
 				</div>
 			</div>
 		</div>
